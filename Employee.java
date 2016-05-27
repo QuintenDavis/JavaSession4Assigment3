eca1ab357ee28b7ecb4154a611c736c611665b94
QuintenDavis <Quinten.R.Davis@gmail.com>
2016-05-27 09:12:44 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,26 @@
+
+public class Employee {
+
+	private String name;
+	private String idNUM;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIdNUM() {
+		return idNUM;
+	}
+	public void setIdNUM(String idNUM) {
+		this.idNUM = idNUM;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
---
+++ TestPerson.java
@@ -0,0 +1,15 @@
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e= new Employee();
+		e.setName("Quinten");
+		e.setAge(29);
+		e.setIdNUM("156783");
+		
+
+	}
+
+}
